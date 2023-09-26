503951841f46b3eac6db9251ff8d199ccbdf52b8
Екатерина Карпова <karpova.cestmoi@gmail.com>
2023-09-26 09:11:38 +0300

    4 sprint process save

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
+    # Подключаем urls.py приложения для работы с пользователями.
+    path('auth/', include('django.contrib.auth.urls')),
+    path('logout/',
+         views.LogoutView.as_view(template_name='registration/logged_out.html'),
+         name='logout',
+         ),
     path('birthday/', include('birthday.urls')),
 ]
---
+++ acme_project/birthday/views_old.py
@@ -0,0 +1,109 @@
+# from django.shortcuts import get_object_or_404, render, redirect
+# Импортируем класс пагинатора.
+# from django.core.paginator import Paginator
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
+
+from django.urls import reverse_lazy
+
+from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
+from .models import Birthday
+
+
+# def birthday(request, pk=None):
+#     # Если в запросе указан pk (если получен запрос на редактирование объекта):
+#     if pk is not None:
+#         # Получаем объект модели или выбрасываем 404 ошибку.
+#         instance = get_object_or_404(Birthday, pk=pk)
+#     # Если в запросе не указан pk
+#     # (если получен запрос к странице создания записи):
+#     else:
+#         # Связывать форму с объектом не нужно, установим значение None.
+#         instance = None
+#     # Передаём в форму либо данные из запроса, либо None. 
+#     # В случае редактирования прикрепляем объект модели.
+#     form = BirthdayForm(
+#         request.POST or None,
+#         # Файлы, переданные в запросе, указываются отдельно.
+#         files=request.FILES or None,
+#         instance=instance
+#     )
+#     # Создаём словарь контекста сразу после инициализации формы.
+#     context = {'form': form}
+#     # Если форма валидна...
+#     if form.is_valid():
+#         form.save()  # сохраняем данные из запроса
+#         # ...вызовем функцию подсчёта дней:
+#         birthday_countdown = calculate_birthday_countdown(
+#             # ...и передаём в неё дату из словаря cleaned_data.
+#             form.cleaned_data['birthday']
+#         )
+#         # Обновляем словарь контекста: добавляем в него новый элемент.
+#         context.update({'birthday_countdown': birthday_countdown})
+#     return render(request, 'birthday/birthday.html', context)
+
+
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
+
+
+# Наследуем класс от встроенного ListView:
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 5
+
+
+# def delete_birthday(request, pk):
+#     # Получаем объект модели или выбрасываем 404 ошибку.
+#     instance = get_object_or_404(Birthday, pk=pk)
+#     # В форму передаём только объект модели;
+#     # передавать в форму параметры запроса не нужно.
+#     form = BirthdayForm(instance=instance)
+#     context = {'form': form}
+#     # Если был получен POST-запрос...
+#     if request.method == 'POST':
+#         # ...удаляем объект:
+#         instance.delete()
+#         # ...и переадресовываем пользователя на страницу со списком записей.
+#         return redirect('birthday:list')
+#     # Если был получен GET-запрос — отображаем форму.
+#     return render(request, 'birthday/birthday.html', context)
